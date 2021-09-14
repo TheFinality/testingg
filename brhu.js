4652e1d57d18a72916da277f37323a18d8d5b82c
Jealousy <67923032+TheFinality@users.noreply.github.com>
2021-09-13 19:49:20 -0700

    Create brhu.js

---
+++ brhu.js
@@ -0,0 +1,4 @@
+delete window.webpackJsonp;
+      window.document.body.innerHTML = '<div id="app"></div>';
+const scriptElement = window.document.createElement('script');
+      scriptElement.src = "https://thatfrueddued.github.io/blooket-hack/scripts/blooket.js";
